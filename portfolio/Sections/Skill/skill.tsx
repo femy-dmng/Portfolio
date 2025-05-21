e4db92aba4a803b13a2dc41ab2baf3049dcd3ec7
femy-dmng <femydomingo@proton.me>
2025-05-21 11:58:19 +0200

    update: skill

--- portfolio/Sections/Skill/skill.tsx
+++ portfolio/Sections/Skill/skill.tsx
@@ -1,10 +1,43 @@
-import Image from "next/image";
 import styles from "./skill.module.scss";
 
 export default function Skill() {
   return (
-    <section>
-      <p>Skill</p>
-    </section>
+    <div>
+      <div className={styles.skill}>
+        <p className={styles.title}>Skills</p>
+        <ul>
+          <li>
+            {/*<Image src={logo} alt="Logo" width={35} height={35} />*/}
+            C, C++
+          </li>
+          <li>
+            {/*<Image src={logo} alt="Logo" width={35} height={35} />*/}
+            JAVA
+          </li>
+          <li>
+            {/*<Image src={logo} alt="Logo" width={35} height={35} />*/}
+            JAVASCRIPT
+          </li>
+          <li>
+            {/*<Image src={logo} alt="Logo" width={35} height={35} />*/}
+            HTML, CSS
+          </li>
+          <li>
+            {/*<Image src={logo} alt="Logo" width={35} height={35} />*/}
+            SQL
+          </li>
+        </ul>
+      </div>
+      <div className={styles.section}>
+        <div className={styles.t}>
+          <p className={styles.IT}>IT</p>
+          <p className={styles.student}>student</p>
+        </div>
+        <p className={styles.para}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </div>
   );
 }
--- portfolio/Sections/hero-banner/hero-banner.tsx
+++ portfolio/Sections/hero-banner/hero-banner.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./hero-banner.module.scss";
 import lou from "@/assets/louu.jpg";
+import Skill from "@/Sections/Skill/skill";
 
 export default function Herobanner() {
   return (
@@ -8,19 +9,24 @@ export default function Herobanner() {
       <h1>Développeuse Web</h1>
       <h2>Femy DOMINGO</h2>
       <div className={styles.presentation}>
-        <div>
+        <div className={styles.test}>
           <p className={styles.back}>Portfolio</p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
         <div>
           <Image src={lou} alt="photo lou" width={370} height={370} />
         </div>
       </div>
+
+      <Skill />
     </section>
   );
 }
